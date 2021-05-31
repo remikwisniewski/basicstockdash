39cf0f153fb4fcdddba8c90dcddb4e3deb677a75
Remi <rwisni736@gmail.com>
2021-05-31 18:12:10 -0500

    update

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Basic stock information dashboard
 
 Links:
 - https://reactjs.org/docs/getting-started.html
+
+
+- create a image folder with logos, etc
+FIX:
+- Header.jsx- logo picture .svg file
--- app/src/Components/Header/Header.jsx
+++ app/src/Components/Header/Header.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import styles from './Header.module.css';
+import logo from '../../images/logo.png';
+
 
 {/*
     CSS MODULES: easier to change CSS in the future, better CSS code management 
     - it is used in the className
-    - it is imported with styles, with: "./filename.module.css"
+    - it is imported with styles, with: "./filename.module.css" as a javascript object
 
 */}
 
 const Header = () => {
-    return <header className={styles.header}> test</header>
+    return (
+        <header className={styles.header}>
+
+        <div className={styles.logoContainer}>
+            <img src={logo} class={styles.logo}/>
+            <span>Basic Stock Dashboard</span>
+        </div>
+
+        <div className={styles.userContainer}>
+            <div className={styles.user}>R</div>
+            <span>Remi</span>
+        </div>
+
+    </header>
+    )
 }
 
 export default Header;
